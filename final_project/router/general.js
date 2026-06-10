629a7c20ca8e094f188e538dbf7a1213f1373a6c
Piktoss <abdellatif.boukoutos@e-polytechnique.ma>
2026-06-10 13:23:55 +0100

    finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,5 +39,45 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
+const axios = require('axios');
 
+// Task 10: Get all books – using async/await with Axios
+public_users.get('/', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/internal/books');
+    res.status(200).send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books" });
+  }
+});
+
+// Task 11: Get book by ISBN – using Promise callbacks
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  axios.get(`http://localhost:5000/internal/isbn/${isbn}`)
+    .then(response => res.status(200).json(response.data))
+    .catch(error => res.status(404).json({ message: "Book not found" }));
+});
+
+// Task 12: Get books by author – using async/await
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+    const response = await axios.get(`http://localhost:5000/internal/author/${author}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({ message: "No books found for this author" });
+  }
+});
+
+// Task 13: Get books by title – using async/await
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const title = req.params.title;
+    const response = await axios.get(`http://localhost:5000/internal/title/${title}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(404).json({ message: "No books found with this title" });
+  }
+});
 module.exports.general = public_users;
